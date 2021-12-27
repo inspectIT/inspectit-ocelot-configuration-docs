7ca0f205cdc630744cbca4b9a4434e2a88f37052
aaronweissler <46028218+aaronweissler@users.noreply.github.com>
2021-12-27 12:40:53 +0100

    Moved project to subfolder for bidirectional gradle importing

--- inspectit-config-docs/app/src/main/java/inspectit/ocelot/configuration/docs/App.java
+++ inspectit-config-docs/app/src/main/java/inspectit/ocelot/configuration/docs/App.java
@@ -5,7 +5,6 @@ package inspectit.ocelot.configuration.docs;
 
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
-import rocks.inspectit.ocelot.config.model.instrumentation.actions.GenericActionSettings;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -18,13 +17,13 @@ public class App {
         DocGenerator docGenerator = new DocGenerator();
         docGenerator.parseConfig(
                 "C:\\Users\\awi\\Documents\\GitHub\\inspectit-ocelot-configuration-docs\\" +
-                        "inspectit_ocelot_repo\\inspectit-ocelot-config\\src\\main\\resources\\rocks\\inspectit\\" +
+                        "inspectit-ocelot\\inspectit-ocelot-config\\src\\main\\resources\\rocks\\inspectit\\" +
                         "ocelot\\config\\default");
 
         docGenerator.generateFullDoc();
         docGenerator.generateHTML();
         docGenerator.saveHTML("C:\\Users\\awi\\Documents\\GitHub\\" +
-                "inspectit-ocelot-configuration-docs\\app\\src\\main\\resources\\");
+                "inspectit-ocelot-configuration-docs\\inspectit-config-docs\\app\\src\\main\\resources\\");
 
         int i = 1;
     }
